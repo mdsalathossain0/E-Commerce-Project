d6a9bd6d4d19f50e3bd63c469c6bf6fb9aae8d63
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-04-21 22:19:10 +0600

    React Route Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider,} from "react-router-dom";
+
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Journal from './pages/Journal';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import MyAccount from './pages/MyAccount';
+import Cart from './pages/Cart';
+import CheckOut from './pages/CheckOut';
+import Error from './pages/Error';
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/contact" element={<Contact />}></Route>
+    <Route path="/journal" element={<Journal />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/signup" element={<Signup />}></Route>
+    <Route path="/myaccount" element={<MyAccount />}></Route>
+    <Route path="/cart" element={<Cart />}></Route>
+    <Route path="/checkout" element={<CheckOut />}></Route>
+    <Route path="/*" element={<Error />}></Route>
+    </>
+  )
+);
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
