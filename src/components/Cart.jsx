fed5a9ddabf7c2ceb761af5acfaec6ac8d1e3e84
mdsalathossain0 <mdsalathosain0@gmail.com>
2025-04-27 19:45:18 +0600

    Cart componnents done

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,18 +4,31 @@ import Image from './Image'
 
 import Cart1 from '../assets/cart1.png'
 import Flex from './Flex'
+import { FaHeart, FaShoppingCart } from 'react-icons/fa'
+import { FaCodeCompare } from 'react-icons/fa6'
 
 const Cart = () => {
   return (
     <Container>
-        <div className='w-[370px] h-[465px]  relative'>
+        <div className='w-[370px] h-[465px]  relative group'>
             <Image className='w-full h-[370px]' src={Cart1}/>
-            <Flex className='justify-between'>
+            <Flex className='justify-between items-center'>
                 <h5 className='text-xl text-second font-bold font-san pt-6 pb-4'>Basic Crew Neck Tee</h5>
-                <h6 className='text-base text-[#767676] font-normal font-san leading-7'>$44.00</h6>
+                <h6 className='text-base text-fivth font-normal font-san leading-7'>$44.00</h6>
             </Flex>
-            <h6 className='text-base text-[#767676] font-normal font-san leading-7'>Black</h6>
+            <h6 className='text-base text-fivth font-normal font-san leading-7'>Black</h6>
             <button className='text-sm text-white font-bold font-san bg-second py-2 px-8 absolute top-5 left-5'>New</button>
+            <div className='w-full bg-white absolute bottom-[0px] left-0 opacity-0 group-hover:opacity-100 group-hover:bottom-[97px] py-6 px-8 duration-500 '>
+              <Flex className='flex-col items-end gap-y-5 '>
+                <p className='text-base text-fivth font-normal font-san hover:font-bold hover:text-second duration-300'>Add to Wish List <FaHeart className='text-second ml-4 inline'/>
+                </p>
+                <p className='text-base text-fivth font-normal font-san hover:font-bold hover:text-second duration-300'>Compare <FaCodeCompare className='text-second ml-4 inline'/>
+                </p>
+                <p className='text-base text-fivth font-normal font-san hover:font-bold hover:text-second duration-300'>Add to Cart <FaShoppingCart className='text-second ml-4 inline'/>
+                </p>
+              </Flex>
+
+            </div>
         </div>
     </Container>
   )
