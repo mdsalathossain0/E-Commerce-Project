7961045c565270272bc0ef4f698afb53f8d26321
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-05-12 16:11:00 +0600

    Error part Done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Container from '../components/Container'
+import Image from '../components/Image'
+import Button from '../components/Button'
+
+import Errorimg from '../assets/error.png'
+import { IoSearchSharp } from 'react-icons/io5'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <section className='py-26'>
+      <Container>
+        <Image src={Errorimg}/>
+        <p className='w-[644px] text-base text-first font-normal font-san leading-8 py-12'>The page you were looking for couldn't be found. The page could be removed or you misspelled the word while searching for it. Maybe try a search?</p>
+        <div className='w-[644px]  relative'>
+          <input className='w-full py-4 px-5 pr-24 border border-sixth'  type="search" placeholder='Type to search'/>
+          <IoSearchSharp className='absolute top-1/2 right-5 -translate-y-1/2'/>
+
+        </div>
+       <div className='pt-15'>
+         <Button text='Back to Home'/>
+       </div>
+      </Container>
+    </section>
   )
 }
 
