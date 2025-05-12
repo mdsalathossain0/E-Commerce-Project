47643d91ff3961632e205cc2720ed8f884feae64
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-05-12 20:07:21 +0600

    sign up part done

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({text, check, placeholder, type,className}) => {
   return (
     <div className='pt-6'>
         <label className='text-base text-second font-bold font-san leading-6' htmlFor={check}>{text} <br />
-     <input id={check} type={type} className={`text-base font-normal  py-4 border-b-2 border-sixth placeholder:text-sm placeholder:text-first placeholder:font-normal ${className}`} placeholder={placeholder}/> <br />
+     <input id={check} type={type} className={`text-base font-normal py-4 px-2 border-b-2 border-sixth placeholder:text-sm placeholder:text-first placeholder:font-normal ${className}`} placeholder={placeholder}/> <br />
     </label>
     </div>
   )
