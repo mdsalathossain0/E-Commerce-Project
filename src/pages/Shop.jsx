8cfa25e2f5d3d14372c7d6a525bdf41eee316ed9
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-05-16 11:31:10 +0600

    React Redux Install

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from '../slice/counterSlice'
 
 const Shop = () => {
+  let dispatch = useDispatch()
+  let data = useSelector((state)=>state.counter.value)
+
+  let handleCLick=()=>{
+    dispatch(increment(1))
+  }
+
   return (
-    <div>Shop</div>
+    <div>
+      
+      <button onClick={handleCLick}>Increment</button>
+      <h1>{data}</h1>
+    </div>
   )
 }
 
